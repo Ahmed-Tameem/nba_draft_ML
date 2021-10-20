0684ae3bc500c52af829f7b0570aeea098f0d8c2
Ahmed Tameem <ahmedwtameem@gmail.com>
2021-10-20 14:21:20 -0700

    Added comments

--- main.py
+++ main.py
@@ -9,27 +9,33 @@ import matplotlib.pyplot as plt
 ######################################################################################################
 
 def scrape():
+    """ Fetch and clean the data to be used in training
+    Inputs:
+        None
+        
+    Returns:
+        X: NumPy array of input samples of shape (n, m)
+        Y: NumPy array (m, ) of known labels
+    """
+
     players_in_nba = players.get_players()
 
-    players_in_nba_names = [player["full_name"] for player in players_in_nba]
+    players_in_nba_names = [player["full_name"] for player in players_in_nba]   #fetch the names of all the players playing in the NBA
 
 
-    path = "D:\\ahmad\\Studies\\Courses\\CPEN 400D\\Misc\\NBA_draft\\nba_draft_ML\\data\\nba_draft_combine_all_years.csv"
+    path = "D:\\ahmad\\Studies\\Courses\\CPEN 400D\\Misc\\NBA_draft\\nba_draft_ML\\data\\nba_draft_combine_all_years.csv"   #Change to path to data
     with open(path, 'r') as data:
         df = pd.read_csv(data)
 
-
-
-    df_cleaned = df.drop(df.columns[[0,1,2,3]], axis = 1, inplace = False)
-
+    df_cleaned = df.drop(df.columns[[0,1,2,3]], axis = 1, inplace = False)  #remove columns with features not used to train such as the player name
     for column in df_cleaned:
-        df_cleaned[column].fillna(df_cleaned[column].mean(), inplace = True)
+        df_cleaned[column].fillna(df_cleaned[column].mean(), inplace = True)    #Fill missing data with the mean of that feature for all players
 
     X = df_cleaned.to_numpy().T
 
     Y = list()
     for player in df["Player"]:
-        Y.append(1 if(player in players_in_nba_names) else 0)
+        Y.append(1 if(player in players_in_nba_names) else 0)   #Check if the player plays in the NBA to create  labels for the data
     Y = np.array(Y)
 
     return X, Y
@@ -44,10 +50,7 @@ def sigmoid(x):
         A NumPy array forthe Sigmoid function applied to x
     """
 
-    ### START CODE HERE ### (~ 1 line of code) ###
-    # YOUR CODE HERE
     s = 1/(1+np.exp(-x))
-    ### END CODE HERE ###
     
     return s
 
@@ -61,15 +64,10 @@ def initialize_parameters(n):
         NumPy Array: the W parameter vector of shape (n, 1)
         float: the b bias paramter
     """
-    ### START CODE HERE ### (~ 2 line of code)
-    # YOUR CODE HERE
+
     w = np.zeros((n,1))
     b = 0.0
-    ### END CODE HERE ###
 
-    assert(w.shape == (n, 1))
-    assert(isinstance(b, float))
-    
     return w, b
 
 def hypothesis(X, w, b):
@@ -82,13 +80,9 @@ def hypothesis(X, w, b):
     Returns:
         NumPy array of shape (1, m) with the hypothesis of each sample
     """
-    A = None
-    
-    ### START CODE HERE ### (~ 1 line of code)
-    # YOUR CODE HERE
+
     x1 =np.matmul(w.T,X)+b
     A = sigmoid(np.matmul(w.T,X)+b)
-    ### END CODE HERE ###
     
     return A
 
@@ -104,12 +98,7 @@ def compute_cost(A, Y):
     """
     
     cost = None
-    
-    ### START CODE HERE ### (~ 1-5 line of code)
-    # YOUR CODE HERE
     cost = float(np.sum(  np.matmul(np.log(A),Y)  +  np.matmul(np.log(1-A),1-Y)  ))/  (-1*(A.shape[1]))
-
-    ### END CODE HERE ###
 
     return cost
 
@@ -125,16 +114,9 @@ def compute_gradients(A, X, Y):
         Two NumPy arrays. One for the cost derivative w.r.t. dw
         and one for the cost derivative w.r.t. db
     """
-    dw = None
-    db = None
-    
-    ### START CODE HERE ### (~ 2-3 line of code)
-    # YOUR CODE HERE
+
     dw = (np.matmul(X,(A-Y).T))  /  (X.shape[1])
     db = np.sum(A-Y)  /  (X.shape[1])
-    ### END CODE HERE ###
-
-    assert(dw.shape == (X.shape[0], 1))
 
     return dw, db
 
@@ -153,29 +135,17 @@ def gradient_descent(X, Y, num_iterations, learning_rate, print_costs=True):
         b: float for trained bias parameter b
         costs: Python list of cost at each iteration
     """
-    w, b = None, None
 
-    ### START CODE HERE ### (~1  line of code)
-    # YOUR CODE HERE
     w, b = initialize_parameters(X.shape[0])
-    ### END CODE HERE ###
-    
-    # We will use a list to store the cost at each iteration
-    # so that we can plot this later for educational purposes
+
     costs = []
     
     for i in range(num_iterations):
-
-        ### START CODE HERE ### (~5  line of code)
-        # YOUR CODE HERE
         A = hypothesis(X,w,b)
         dw, db = compute_gradients(A, X, Y)
         w = w - learning_rate * dw
         b = b - learning_rate * db
         cost = compute_cost(A, Y)
-        ### END CODE HERE ###
-        
-        # Convert and save the cost at this iteration
         costs.append(cost)
         
         # Print cost after ever 5000 iterations
@@ -185,7 +155,20 @@ def gradient_descent(X, Y, num_iterations, learning_rate, print_costs=True):
     return w, b, costs
 
 def train(X, Y):
+    """ Use the Logistic Regression Model to make predictions
+    
+    Inputs:
+        X: NumPy array (n, m) of feature data
+        Y: NumPy array (m, ) of known labels
+    
+    Returns:
+        w: NumPy array of parameters with shape (n, 1)
+        b: float for the bias parameter
+        costs: Float list of all the costs associated with each iteration
+    """
+
     w, b, costs = gradient_descent(X, Y, 15000, 0.0000000113)
+
     return w, b, costs
 
 def predict(X, w, b):
@@ -199,13 +182,10 @@ def predict(X, w, b):
     Returns:
         NumPy array (m, ) of predictions.  Values are 0 or 1.
     """
-    y_pred = None
 
-    ### START CODE HERE ### (~1  line of code)
-    # YOUR CODE HERE
+    y_pred = None
     y_pred = np.rint(hypothesis(X, w, b))
-    ### END CODE HERE ###
-    
+
     return y_pred
 
 def compute_accuracy(X, Y, w, b):
@@ -222,15 +202,22 @@ def compute_accuracy(X, Y, w, b):
         float between 0 and 1 denoting the accuracy of the
         Logistic Regression model 
     """
+
     accuracy = None
-    
-    ### START CODE HERE ### (1-2  line of code)
-    # YOUR CODE HERE
     accuracy = np.sum(Y == predict(X, w, b))/X.shape[1]
-    ### END CODE HERE ###
+
     return accuracy
 
 def plot_costs(costs):
+    """ Plot the cost as a function of the number of iterations
+    Inputs:
+        costs: Float list of all the costs associated with each iteration
+        
+    Returns:
+        X: NumPy array of input samples of shape (n, m)
+        Y: NumPy array of the labels of shape (m, )
+    """
+
     plot2 = plt.figure(2)
     plt.plot(costs)
     plt.xlabel('Iteration')
@@ -247,6 +234,7 @@ Y_train = Y[:456]
 Y_test = Y[456:]
 
 w, b, costs = train(X_train, Y_train)
+
 accuracy = compute_accuracy(X_test, Y_test, w, b)
 print('Accuracy: {0}%'.format(accuracy*100))
 
